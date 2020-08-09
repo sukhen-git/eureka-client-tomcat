dc3974a30a873a20098aac4261fd1c067ad8148a
bhattacharjee.sukhen@gmail.com <bhattacharjee.sukhen@gmail.com>
2020-08-09 20:50:41 +0530

    Initial Commit

---
+++ src/test/java/com/test/sample/EurekaClientTomcatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaClientTomcatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
